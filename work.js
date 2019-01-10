6146c446e4adbc17fae526683f6e59955b8c62dc
SolaJr <35401733+SolaJr@users.noreply.github.com>
2019-01-09 23:08:18 -0600

    Add files via upload

--- work.js
+++ work.js
@@ -1,6 +1,7 @@
-
+﻿
 var works1 = [
 	{
+ 
 		title: "",
 		pic: "img/inProgress.jpg",
 		caption: "Cryptaceous Creations",
@@ -9,8 +10,11 @@ var works1 = [
 		url: "Cryptaceous_Creations.html",
 		link: "https://blog.prototypr.io/talent-marketplace-ux-case-study-648d26235047",
 		action: "Read case study"
-	},
+	}
+ 
+ ,
 	{
+ 
 		title: "",
 		pic: "img/inProgress.jpg",
 		caption: " ",
@@ -20,31 +24,42 @@ var works1 = [
 		link: " ",
 		action: "Read case study"
 	}
-];
+ 
+ 
+]
+;
 
 var works2 = [
 	{
+ 
 		title: "Facebook Local Challenge",
-		pic: "FacebookLocal.png",
+		pic: "img/mockups/FacebookLocal.png",
 		caption:"Facebook Local Challenge" ,
 		type: "In an attempt to test my knolwedge, I created a 48 hour prototyping challenge.",
-		desc: "In an attempt to test my knolwedge, I created a 48 hour prototyping challenge.",
-		url: "FacebookL.html",
+		desc: "This project is a simple 48 hour prototyping challenge I undertook in December. The goal was to analyze the app, Facebook Local, and recreate a framework that could address some usablity concerns found in the design. User feedback was the source.",
+		url: "FBLocal2.html",
 		link: "https://www.figma.com/proto/LAk5Z0ZvGv7bY2kmQOAcWNvz/Untitled?node-id=0%3A1&scaling=scale-down",
 		action: "View Case Study!",
 		nda: false
-	},{
-		title: "Cryptaceous Creations",
-		pic: "pics/projects/dino2.jpg",
-		caption:"Cyptaceous Creations ",
-		type: "UX Designer & App Developer",
-		desc: "Say Hello to Hong Kong's first ever Blockchain based Augmented Reality game!",
-		url: "Cryptaceous_Creations.html",
-		link: "https://devpost.com/software/cryptaceous-creations",
-		action: "Read case study",
+	}
+ 
+ ,{
+ 
+		title: "VestaGoGo",
+		pic: "img/mockups/VestaGoGO.png",
+		caption:"VestaGoGo" ,
+		type: "In an attempt to test my knolwedge, I created a 48 hour prototyping challenge.",
+		desc: "Navigation can be difficult when their are many things you need to keep your eyes and ears aware of. So we developed an app to assist blind and visually impaired civilians.",
+		url: "VestaGoGo.html",
+		link: "https://vimeo.com/309193246",
+		action: "View Case Study!",
 		nda: false
-	},
+	}
+ 
+ 
+ ,
 	 /*{
+ 
 		title: "Website",
 		pic: "Reinvent Facebook Local_files/image006.jpg",
 		caption:"Website Design Process",
@@ -54,44 +69,94 @@ var works2 = [
 		link: " ",
 		action: "View Case Study",
 		nda: false
-	},*/
+	}
+ 
+ ,*/
 		{
+ 
 		title: "PhobiApp",
 		pic: "PhobiApp/PhobiApp/background06.jpg",
 		caption: "PhobiApp ",
 		type: "Product Designer and Presenter",
-		desc: " PhobiApp is VR app that applies prinicples of modern psychology to treat phobias.",
-		url: "Team_8_phobiApp-_1.html" ,
+		desc: "Glossophobia (the fear of public speaking) is the most common phobia high school and college students. This start up is combining modern psychology with Virtual Reality to help students conquor this fear.",
+		url: "phobi.html" ,
 		link: " https://youtu.be/RV2tXc7FFSk?t=6244 ",
 		action: "Read case study",
 		nda: false
-	},{
+	}, {
+ 
+		title: "Century Singers Website Design",
+		pic: "img/mockups/Scaled/Note.png",
+		caption: "Centruy Singers Website Mockup ",
+		type: "UX Designer and Web Developer",
+		desc: "Century Singers choir undergone a vareity of web designs. Guess what? By touching the black icon below, you can see a sneak peak of the new design as well!",
+		url: "http://centurysingers.tamu.edu/" ,
+		link: "https://solajr.github.io/Century-Singers-Website-Prototype/ ",
+		action: "View Current Website",
+		nda: false
+	}
+
+ 
+ ,{
+ 
 		title: "Dash AR ",
-		pic: "pics/Projects/DashAr.png",
+		pic: "img/mockups/DashAR.png",
 		caption: "Dash - AR ",
 		type: "Product Designer and Presenter",
-		desc: " Can exercise ever be fun for a beginner? Maybe if you add AR.",
-		url: "Aggie Invent Team Dash Final/Aggie Invent Team Dash Final.html" ,
+		desc: "Getting people to try fitness app for the first time is a fairly simple task. Getting them to try it a secound or third times is where the true problem lies! This mobile AR App creates a virtual world where users can race against their friends and themselves an compete for prizes such as a month of free Netflix.",
+		url: "Dash-AR.html" ,
 		link: "https://www.youtube.com/watch?v=o6X6eoYglyA ",
 		action: "Read case study",
 		nda: false
-	},{
+	}
+ 
+ ,{
+ 
 		title: "Emerson Compact Prover",
-		pic: "pics/Projects/Emerson2.png",
+		pic: "img/mockups/Emerson.png",
 		caption: "The Emerson Electric Companies Daniel Compact Prover",
 		type: "Design Engineer Intern",
-		desc: "Redesiging the Daniel Compact Prover to improve the customer's user experience in addition to maintenance & accuracy.",
-		url: "Emerson_Prover.html",
+		desc: "The Daniel Compact Prover has not undergone significant changes in many years and is in need of an update to remain competitive in the market. So during my internship, I was tasked with developing a prover design that would make it stand out against other competing provers that are currently in the market.",
+		url: "Emerson.html",
 		link: "",
 		action: "Read case study",
-		nda: true
+		nda: false
+	}
+ ,{
+ 
+		title: "Balloon Grid",
+		pic: "img/mockups/BalloonGrid.png",
+		caption: "Balloon Grid",
+		type: "Researcher and Product Designer",
+		desc: "Applications of modern shape memory polymers can be used to improve customer experiences in the construction industry.",
+		url: "BalloonGrid.html",
+		link: "BalloonGrid.html",
+		action: "Read case study",
+		nda: false
+	},
+	{
+ 
+		title: "Cryptaceous Creations",
+		pic: "img/mockups/Creations2.png",
+		caption:"Cyptaceous Creations ",
+		type: "UX Designer & Unity Developer",
+		desc: "Say hello to the world's first ever educational Blockchain based mobile Augmented Reality Game! Wow, that's a mouth full. Hong Kong was looking for ways to teach kids about Blockchain, so we developed a mobile fossil hunting game that created uniquely colored dinosaurs to teach the fundamentals of how to use Blockchain.",
+		url: "Cryptaceous_Creations.html",
+		link: "https://devpost.com/software/cryptaceous-creations",
+		action: "Read case study",
+		nda: false
 	}
 
-];
+
+ 
+
+]
+;
 
 
 var works4 = [
 	{
+ 
 		title: "",
 		pic: "profile.jpg",
 		caption:"Product Design Experience",
@@ -102,64 +167,84 @@ var works4 = [
 		action: "Who is Sola!",
 		nda: false
 	}
+ 
+ 
 	
-	];
+	]
+;
 var works3 = [
 	
 	{
-		title: "Consumer Electronic Product Design ",
+ 
+		title: "Pluto : Electronic Rover ",
 		pic: "pics/Projects/ElecDesign1.png",
 		caption: "International Robotic Development!",
 		type: "Robotic Product Design Apprentice Program  ",
-		desc: "During an international experience in Hong Kong, I got a chance to work with one of the best robotic product design universities in the world.",
+		desc: "As a part of a exchange program with the University of Hong Kong, I got the chance to design an electronic rover from scratch.This project was an series of small electronic product design projects that came together to develop an autonomus rover from scratch. It was constructed during my exchange program in Hong Kong.",
 		url:  "Elec.html",
 		link: "",
 		action: "Read case study",
 		nda: false
-	},
+	}
+ 
+ ,{
+ 
+		title: "ScholarFish ",
+		pic: "img/mockups/ScholarFish2.png",
+		caption: "ScholarFish",
+		type: "Lead Software Developer",
+		desc: "Scholarfish is a computer program that was designed to help high schoolers identify scholarships that they had a strong chance of winning based on their academic status.",	
+		url: "ScholarFish.html",
+		link: "https://www.kbtx.com/content/news/Texas-AM-students-to-help-FEMA-with-site-inspections-465545153.html",
+		action: "View Project",
+		nda: true 
+	}
+ 
+ ,
+
 	{
+ 
 		title: "Federal Emergency Management Agency",
 		pic: "pics/Projects/Fema2.png",
 		caption: "FEMA Infrastructure Internship ",
 		type: "Emergency Management Operations Intern",
 		desc: "After a crippling assault by Hurricane Harvey on Houston, something had to be done.",	
-		url: "Fema.html",
+		url: "https://www.kbtx.com/content/news/Texas-AM-students-to-help-FEMA-with-site-inspections-465545153.html",
 		link: "https://www.kbtx.com/content/news/Texas-AM-students-to-help-FEMA-with-site-inspections-465545153.html",
-		action: "View Project",
+		action: "View News Report",
 		nda: false
-	},
-	{
-		title: "Balloon Grid",
-		pic: "pics/Projects/BalloonGrid.png",
-		caption: "Balloon Grid",
-		type: "Applications of modern shape memory polymers can be used to improve customer experiences in the construction industry.",
-		url: "BalloonGrid.html",
-		link: "",
-		action: "View Project",
-		nda: false
-	},{
+	}
+  
+ ,{
+ 
 		title: "Resume",
-		pic: "Reinvent Facebook Local_files/image006.jpg",
-		caption:"Resume",
+		pic: "pics/Product.png",
+		caption:"View my Work Experience here",
 		type: " ",
 		desc: " ",
-		url: "My_Product_Design_Experience.html",
+		url: "https://tx.ag/SolaStudios",
 		link: " ",
-		action: "Who is Sola!",
+		action: "Click Here",
 		nda: false
-	},	{
-		title: "Summary of Engineering Projects",
-		pic: "img/inProgress.jpg",
-		caption: "Summary of Engineering Projects",
-		type: "Rather then clicking though each project, this will contain a summary of all of my works.",
+	}
+ 
+ ,	{
+ 
+		title: "Misc Projects",
+		pic: "pics/Tamu.png",
+		caption: "Misc Products",
+		type: "Here is a link to more engineering products I worked on",
 		url: "https://sites.google.com/view/olusolababatunde/projects",
 		link: "https://sites.google.com/view/olusolababatunde/projects",
 		action: "View Project",
 		nda: false
-	},
+	}
+ 
+ ,
 
 	
-];
+]
+;
 
 
 
